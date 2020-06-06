1d9f73589dc41893e2f52671b5c409c77cd2e8a4
Keita39 <takoyaki.nao39@gmail.com>
2020-06-06 23:09:47 +0900

    動画の詳細ページを追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'pages/home'
   get '/posts/new', to: 'posts#new'
   post '/posts', to: 'posts#create'
-  resources :posts, only: %i(new create index)
+  resources :posts, only: %i(index new create show)
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
