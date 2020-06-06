d16d15948344773cfe34a7902d786d9c85142682
Keita39 <takoyaki.nao39@gmail.com>
2020-06-06 19:22:01 +0900

    Postテーブル追加 CarrierWaveのアップロード機能の追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_28_134228) do
+ActiveRecord::Schema.define(version: 2020_06_06_101557) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.string "video"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "snippets", force: :cascade do |t|
     t.string "title"
